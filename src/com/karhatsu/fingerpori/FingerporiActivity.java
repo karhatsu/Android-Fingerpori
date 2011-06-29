e0f47136dda970594240379f3b23d2195c5931a3
Henri Karhatsu <henri@karhatsu.com>
2011-06-29 21:35:55 +0300

    next button

--- src/com/karhatsu/fingerpori/FingerporiActivity.java
+++ src/com/karhatsu/fingerpori/FingerporiActivity.java
@@ -1,15 +1,5 @@
 package com.karhatsu.fingerpori;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,10 +10,7 @@ import android.widget.Toast;
 
 public class FingerporiActivity extends Activity {
 	private static final String FINGERPORI_URL = "http://www.hs.fi/fingerpori";
-	private static final String IMAGE_URL_REGEX = "(http://www.hs.fi/kuvat/iso_webkuva/[0-9]*.(gif|jpeg))";
-	private static final String PREV_URL_REGEX = "previous.*(http://www.hs.fi/fingerpori/[0-9]+).*>\\s*Edellinen";
-
-	private String prevUrl;
+	private ImageSource imageSource = new ImageSource(FINGERPORI_URL);
 
 	public FingerporiActivity() {
 	}
@@ -33,7 +20,8 @@ public class FingerporiActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		definePrevButton();
-		readUrlsAndShowImage(FINGERPORI_URL);
+		defineNextButton();
+		loadImage();
 	}
 
 	private void definePrevButton() {
@@ -41,8 +29,9 @@ public class FingerporiActivity extends Activity {
 		button.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				if (prevUrl != null) {
-					readUrlsAndShowImage(prevUrl);
+				if (imageSource.getPrev() != null) {
+					imageSource = imageSource.getPrev();
+					loadImage();
 				} else {
 					showToast("Edellistä Fingerporia ei ole saatavilla");
 				}
@@ -51,69 +40,29 @@ public class FingerporiActivity extends Activity {
 		});
 	}
 
-	private void readUrlsAndShowImage(String htmlUrl) {
-		String imageUrl = readImageAndPrevUrls(htmlUrl);
-		loadImage(imageUrl);
+	private void defineNextButton() {
+		Button button = (Button) findViewById(R.id.nextButton);
+		button.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (imageSource.getNext() != null) {
+					imageSource = imageSource.getNext();
+					loadImage();
+				} else {
+					showToast("Seuraavaa Fingerporia ei ole saatavilla");
+				}
+			}
+
+		});
 	}
 
 	private void showToast(String text) {
 		Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
 	}
 
-	private void loadImage(String imageUrl) {
+	private void loadImage() {
 		WebView webView = (WebView) findViewById(R.id.webView);
-		webView.loadUrl(imageUrl);
+		webView.loadUrl(imageSource.getImageUrl());
 	}
 
-	private String readImageAndPrevUrls(String fullHtmlUrl) {
-		String fullHtml = loadFullHtml(fullHtmlUrl);
-		String imageUrl = fullHtmlUrl; // fallback to full html page
-		if (fullHtml == null) {
-			prevUrl = null;
-			return imageUrl;
-		}
-		try {
-			imageUrl = parseImageUrl(fullHtml);
-		} catch (Exception e) {
-		}
-		try {
-			prevUrl = parsePrevUrl(fullHtml);
-		} catch (Exception e) {
-			prevUrl = null;
-		}
-		return imageUrl;
-	}
-
-	private String loadFullHtml(String fullHtmlUrl) {
-		try {
-			HttpClient client = new DefaultHttpClient();
-			HttpGet request = new HttpGet(fullHtmlUrl);
-			HttpResponse response = client.execute(request);
-			BufferedReader rd = new BufferedReader(new InputStreamReader(
-					response.getEntity().getContent()));
-			String line = "";
-			StringBuilder sb = new StringBuilder();
-			while ((line = rd.readLine()) != null) {
-				sb.append(line);
-			}
-			return sb.toString();
-		} catch (Exception e) {
-			return null;
-		}
-	}
-
-	private String parseImageUrl(String html) {
-		return parseUrl(html, IMAGE_URL_REGEX);
-	}
-
-	private String parsePrevUrl(String html) {
-		return parseUrl(html, PREV_URL_REGEX);
-	}
-
-	private String parseUrl(String html, String regex) {
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(html);
-		matcher.find();
-		return matcher.group(1);
-	}
 }
---
+++ src/com/karhatsu/fingerpori/ImageSource.java
@@ -0,0 +1,94 @@
+package com.karhatsu.fingerpori;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+public class ImageSource {
+	private static final String IMAGE_URL_REGEX = "(http://www.hs.fi/kuvat/iso_webkuva/[0-9]*.(gif|jpeg))";
+	private static final String PREV_URL_REGEX = "previous.*(http://www.hs.fi/fingerpori/[0-9]+).*>\\s*Edellinen";
+
+	private String fullHtmlUrl;
+	private String imageUrl;
+	private ImageSource next;
+	private ImageSource prev;
+
+	public ImageSource(String htmlUrl) {
+		this.fullHtmlUrl = htmlUrl;
+	}
+
+	private ImageSource(String htmlUrl, ImageSource next) {
+		this.fullHtmlUrl = htmlUrl;
+		this.next = next;
+	}
+
+	public String getImageUrl() {
+		if (imageUrl == null) {
+			readImageUrlAndCreatePrevSources();
+		}
+		return imageUrl;
+	}
+
+	private void readImageUrlAndCreatePrevSources() {
+		String fullHtml = loadFullHtml(fullHtmlUrl);
+		imageUrl = fullHtmlUrl; // fallback to full html page
+		if (fullHtml == null) {
+			return;
+		}
+		try {
+			imageUrl = parseImageUrl(fullHtml);
+		} catch (Exception e) {
+		}
+		try {
+			prev = new ImageSource(parsePrevUrl(fullHtml), this);
+		} catch (Exception e) {
+		}
+	}
+
+	private String loadFullHtml(String fullHtmlUrl) {
+		try {
+			HttpClient client = new DefaultHttpClient();
+			HttpGet request = new HttpGet(fullHtmlUrl);
+			HttpResponse response = client.execute(request);
+			BufferedReader rd = new BufferedReader(new InputStreamReader(
+					response.getEntity().getContent()));
+			String line = "";
+			StringBuilder sb = new StringBuilder();
+			while ((line = rd.readLine()) != null) {
+				sb.append(line);
+			}
+			return sb.toString();
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	private String parseImageUrl(String html) {
+		return parseUrl(html, IMAGE_URL_REGEX);
+	}
+
+	private String parsePrevUrl(String html) {
+		return parseUrl(html, PREV_URL_REGEX);
+	}
+
+	private String parseUrl(String html, String regex) {
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(html);
+		matcher.find();
+		return matcher.group(1);
+	}
+
+	public ImageSource getPrev() {
+		return prev;
+	}
+
+	public ImageSource getNext() {
+		return next;
+	}
+}
