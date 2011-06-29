a21a936d6d9a0e3a8f5053ad61918b17f7f9248d
Henri Karhatsu <henri@karhatsu.com>
2011-06-29 21:16:31 +0300

    gif vs. jpeg fixed

--- src/com/karhatsu/fingerpori/FingerporiActivity.java
+++ src/com/karhatsu/fingerpori/FingerporiActivity.java
@@ -1,13 +1,11 @@
 package com.karhatsu.fingerpori;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -22,7 +20,7 @@ import android.widget.Toast;
 
 public class FingerporiActivity extends Activity {
 	private static final String FINGERPORI_URL = "http://www.hs.fi/fingerpori";
-	private static final String IMAGE_URL_REGEX = "(http://www.hs.fi/kuvat/iso_webkuva/[0-9]*.gif)";
+	private static final String IMAGE_URL_REGEX = "(http://www.hs.fi/kuvat/iso_webkuva/[0-9]*.(gif|jpeg))";
 	private static final String PREV_URL_REGEX = "previous.*(http://www.hs.fi/fingerpori/[0-9]+).*>\\s*Edellinen";
 
 	private String prevUrl;
@@ -68,32 +66,40 @@ public class FingerporiActivity extends Activity {
 	}
 
 	private String readImageAndPrevUrls(String fullHtmlUrl) {
-		String imageUrl;
+		String fullHtml = loadFullHtml(fullHtmlUrl);
+		String imageUrl = fullHtmlUrl; // fallback to full html page
+		if (fullHtml == null) {
+			prevUrl = null;
+			return imageUrl;
+		}
 		try {
-			String fullHtml = loadFullHtml(fullHtmlUrl);
 			imageUrl = parseImageUrl(fullHtml);
+		} catch (Exception e) {
+		}
+		try {
 			prevUrl = parsePrevUrl(fullHtml);
 		} catch (Exception e) {
-			e.printStackTrace();
-			imageUrl = fullHtmlUrl; // fallback to full html page
 			prevUrl = null;
 		}
 		return imageUrl;
 	}
 
-	private String loadFullHtml(String fullHtmlUrl) throws IOException,
-			ClientProtocolException {
-		HttpClient client = new DefaultHttpClient();
-		HttpGet request = new HttpGet(fullHtmlUrl);
-		HttpResponse response = client.execute(request);
-		BufferedReader rd = new BufferedReader(new InputStreamReader(response
-				.getEntity().getContent()));
-		String line = "";
-		StringBuilder sb = new StringBuilder();
-		while ((line = rd.readLine()) != null) {
-			sb.append(line);
+	private String loadFullHtml(String fullHtmlUrl) {
+		try {
+			HttpClient client = new DefaultHttpClient();
+			HttpGet request = new HttpGet(fullHtmlUrl);
+			HttpResponse response = client.execute(request);
+			BufferedReader rd = new BufferedReader(new InputStreamReader(
+					response.getEntity().getContent()));
+			String line = "";
+			StringBuilder sb = new StringBuilder();
+			while ((line = rd.readLine()) != null) {
+				sb.append(line);
+			}
+			return sb.toString();
+		} catch (Exception e) {
+			return null;
 		}
-		return sb.toString();
 	}
 
 	private String parseImageUrl(String html) {
